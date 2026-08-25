8de36eaa4bfb3aff5e3eb28fc2110e11d27b81da
Pratyush Bidare <pratyush@Pratyushs-MacBook-Air.local>
2026-08-25 23:48:02 +0530

    fix: revert glance widget intent error

--- app/src/main/java/com/hourlock/app/widget/HourLockWidget.kt
+++ app/src/main/java/com/hourlock/app/widget/HourLockWidget.kt
@@ -7,8 +7,7 @@ import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
-import androidx.glance.appwidget.action.actionStartActivity
-import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.cornerRadius
